33b48b84cdc3a0d6393ac5785a1599e1a9e1da7f
julien shearer <julienshearer96@gmail.com>
2020-08-20 19:24:54 +1000

    change 1

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,8 +7,9 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("test")
-    }
+		Debug.Log("test")
+
+	}
 
     // Update is called once per frame
     void Update()
