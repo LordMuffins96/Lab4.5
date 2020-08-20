6aeb6a09d45e65bf1dddfda7068492ac4d211f27
julien shearer <julienshearer96@gmail.com>
2020-08-20 22:22:56 +1000

    90 percent

---
+++ Assets/PrintAndHide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    public Renderer rend;
+    [HideInInspector]
+    public int i= 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.name + ": " + i);
+        i += 1;
+    }
+}
